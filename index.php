a59f636823f3f7d9549d98c4ce47fbabf2eb38a5
dlshoong <dlshoong@users.noreply.github.com>
2019-09-09 13:57:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD']
+
+// Process only when method is post
+if($method == "POST"){
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+
+    $text = $json->result->parameters->text;
+
+    switch ($text) {
+        case 'hi':
+            $speech = "Hi, Nice to meet you :)";
+            break;
+
+        case 'bye'
+         $speech = "Bye Seng Hoong :)"   
+         break;
+        
+        case "Rivotril"
+            $speech = "There are 10.";
+        break;
+
+        default:
+            $speech = "Sorrt, I didnt get that. Please ask another question.";
+            break;
+    }
+
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
+
+
+}else{
+    echo "Method not allowed";
+}
+
+
+
+?>
