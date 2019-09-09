ae4a0e31a89e4e820dacac282531b57bd80f1143
dlshoong <dlshoong@users.noreply.github.com>
2019-09-09 14:30:18 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ if ($method == "POST"){
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
-    $text = $json->result->parameters->text;
+    $text = $json->result->parameters->Product;
 
     switch ($text) {
         case 'hi':
-            $speech = "Hi, Nice to meet you";
+            $speech = "Hi, Nice to meet you :) ";
             break;
 
         case 'bye':
-            $speech = "Bye" ;
+            $speech = "Bye :)" ;
             break;
         
         case "Rivotril":
@@ -23,7 +23,7 @@ if ($method == "POST"){
             break;
 
         default:
-            $speech = "Sorrt, I didnt get that. Please ask another question.";
+            $speech = "Sorry, I didnt get that. Please ask another question.";
             break;
     }
 
